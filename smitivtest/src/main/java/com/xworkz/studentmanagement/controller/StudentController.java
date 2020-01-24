adb22ae706cb38da9fe32909b5bff390a56aea27
kabirsmoker <kabirsn786@gmail.com>
2020-01-25 01:01:05 +0530

    deleted unused file

--- smitivtest/src/main/java/com/xworkz/studentmanagement/controller/StudentController.java
+++
@@ -1,5 +0,0 @@
-package com.xworkz.studentmanagement.controller;
-
-public class StudentController {
-
-}
--- smitivtest/src/main/java/com/xworkz/studentmanagement/dto/StudentDTO.java
+++
@@ -1,5 +0,0 @@
-package com.xworkz.studentmanagement.dto;
-
-public class StudentDTO {
-
-}
